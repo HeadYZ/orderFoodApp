85b631acf4be6cda3e275b5c68f0fa77f610789e
Patryk <krasowski.p@outlook.com>
2024-01-26 19:04:17 +0100

    change onClose prop

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -18,7 +18,11 @@ export default function Cart() {
 		userProgressCtx.showCheckout()
 	}
 	return (
-		<Modal className='cart' open={userProgressCtx.progress === 'cart'} onClose={handleCloseCart}>
+		<Modal
+			className='cart'
+			open={userProgressCtx.progress === 'cart'}
+			onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}
+		>
 			<h2>Your Cart</h2>
 			<ul>
 				{cartCtx.items.map(item => (
