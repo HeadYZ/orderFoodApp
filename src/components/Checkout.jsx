4f4c6580a1e9d3e70dbc9894c8d29e38052522a0
Patryk <krasowski.p@outlook.com>
2024-01-26 18:54:01 +0100

    add context to Checkout component

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,16 +1,36 @@
 import { useContext } from 'react'
 import Modal from './UI/Modal.jsx'
 import CartContext from '../store/CartContext.jsx'
+import Input from './UI/Input.jsx'
 import { currencyFormatter } from '../util/formatting.js'
+import Button from './UI/Button.jsx'
+import UserPogressContext from '../store/UserProgressContext.jsx'
 
 export default function Checkout() {
 	const cartCtx = useContext(CartContext)
 	const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0)
+	const userProgressCtx = useContext(UserPogressContext)
+	function handleClose() {
+		userProgressCtx.hideCheckout()
+	}
 	return (
-		<Modal>
+		<Modal open={userProgressCtx.progress === 'checkout'}>
 			<form>
 				<h2>Checkout</h2>
 				<p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
+				<Input label='Full Name' type='text' id='full-name' />
+				<Input label='E-MAIL Address' type='email' id='email' />
+				<Input label='Street' type='text' id='street' />
+				<div className='control-row'>
+					<Input label='Postal Code' type='text' id='postal-code' />
+					<Input label='City' type='text' id='city' />
+				</div>
+				<p className='modal-actions'>
+					<Button type='button' textOnly onClick={handleClose}>
+						Close
+					</Button>
+					<Button>Submit Order</Button>
+				</p>
 			</form>
 		</Modal>
 	)
